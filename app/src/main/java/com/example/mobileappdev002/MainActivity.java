2cd3609913383af6fed741c31030aead212b9da4
iamshaveenvf <shaveenfernando1@gmail.com>
2022-10-25 21:49:35 +0530

    activity 2 added

--- app/src/main/java/com/example/mobileappdev002/MainActivity.java
+++ app/src/main/java/com/example/mobileappdev002/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.mobileappdev002;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView text;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btnOk);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text = findViewById(R.id.txtHello);
+                text.setText("Bye");
+            }
+        });
     }
+
 }
